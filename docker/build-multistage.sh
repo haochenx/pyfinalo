964cd9a0e5eb8d7bc39116b169b520e62cac24d1
Haochen M. Kotoi-Xie <haochenx@acm.org>
2025-06-16 01:27:22 +0900

    promote multi-stage as default Docker build

--- docker/build-multistage.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "Building pyfinalo multi-stage Docker image..."
-docker build -f docker/Dockerfile.multistage -t pyfinalo:multistage .
-echo "Multi-stage build complete! Run with: docker run -it pyfinalo:multistage"
--- docker/build.sh
+++ docker/build.sh
@@ -4,3 +4,4 @@ set -e
 echo "Building pyfinalo Docker image..."
 docker build -f docker/Dockerfile -t pyfinalo .
 echo "Build complete! Run with: docker run -it pyfinalo"
+echo "Image size: $(docker images pyfinalo:latest --format "{{.Size}}")"
