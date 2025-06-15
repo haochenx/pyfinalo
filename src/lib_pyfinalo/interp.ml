c15eeb1e195b188da5db13d467e069b10800f65b
Haochen M. Kotoi-Xie <haochenx@acm.org>
2025-06-15 14:35:35 +0900

    add TypeChecking

--- src/lib_pyfinalo/interp.ml
+++ src/lib_pyfinalo/interp.ml
@@ -42,3 +42,48 @@ end
 (* module signature check *)
 module _ : Lang.LangUntyped = UntypedAstInterp
 module _ : Lang.LangBase = UntypedAstInterp
+
+module type Consumer = sig
+  module Tgt : Lang.LangBase
+  type r
+  val consume : _ Tgt.repr -> r
+end
+
+type 'r consumer = (module Consumer with type r = 'r)
+
+module TypeChecking (Tgt : Lang.LangBase) = struct
+  type 't repr =
+    | Int_typed : int Tgt.repr -> _ repr
+    | String_typed : string Tgt.repr -> _ repr
+    | Ill_typed : _ repr
+
+  let int x = Int_typed (Tgt.int x)
+  let str s = String_typed (Tgt.str s)
+
+  let add e1 e2 =
+    match e1, e2 with
+    | (Int_typed e1),
+      (Int_typed e2)
+      -> Int_typed (Tgt.add e1 e2)
+    | _ -> Ill_typed
+
+  let mul e1 e2 =
+    match e1, e2 with
+    | (Int_typed (e1 : int Tgt.repr)),
+      (Int_typed (e2 : int Tgt.repr))
+      -> Int_typed (Tgt.mul e1 e2)
+    | _ -> Ill_typed
+
+  let len = function
+    | (String_typed (e : string Tgt.repr))
+      -> Int_typed (Tgt.len e)
+    | _ -> Ill_typed
+
+  let pp_repr (vppf : Format.formatter -> _ Tgt.repr -> unit) ppf : _ repr -> unit = function
+    | Int_typed v -> Format.fprintf ppf "%a : int" vppf (Obj.magic v)
+    | String_typed v -> Format.fprintf ppf "%a : string" vppf (Obj.magic v)
+    | Ill_typed -> Format.fprintf ppf "ill-typed"
+
+end
+
+module _ (Tgt : Lang.LangBase) : Lang.LangUntyped = TypeChecking(Tgt)
--- src/lib_pyfinalo/test/lib_pyfinalo_unit_tests.ml
+++ src/lib_pyfinalo/test/lib_pyfinalo_unit_tests.ml
@@ -69,4 +69,41 @@ let () =
             );
           );
         ];
+
+      "TypeChecking",
+      let module TypeCheckingDirectValInterp = TypeChecking(DirectValInterp) in
+      let module Checked = TypeCheckingDirectValInterp in
+      let test_type_checked = testable (Checked.pp_repr pp_irepr) (=) in
+      [
+          "lit", `Quick, (
+            fun () ->
+            check test_type_checked "litint" (Int_typed (V_int 3)) Checked.(
+              (int 3)
+            );
+            check test_type_checked "litstr" (String_typed (V_str "hello")) Checked.(
+              (str "hello");
+            );
+          );
+          "arith-welltyped", `Quick, (
+            fun () ->
+            check test_type_checked "add1" (Int_typed (V_int 7)) Checked.(
+              add (int 3) (int 4)
+            );
+            check test_type_checked "mul1" (Int_typed (V_int 26)) Checked.(
+              mul (int 2) (int 13)
+            );
+            check test_type_checked "mul2" (Int_typed (V_int 26)) Checked.(
+              mul (len (str "hi")) (int 13)
+            );
+          );
+          "arith-illtyped", `Quick, (
+            fun () ->
+            check test_type_checked "add1" (Ill_typed) Checked.(
+              add (int 3) (str "hello")
+            );
+            check test_type_checked "add2" (Ill_typed) Checked.(
+              mul (len (int 2)) (int 13)
+            );
+          );
+        ];
     ]
